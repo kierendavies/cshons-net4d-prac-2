3cecc9d7277b03f0b190f073a4fa48763f2d5971
Kieren Davies <kieren@kdavi.es>
2016-05-28 13:56:46 +0200

    There's a seventh node

--- iperf.sh
+++ iperf.sh
@@ -3,7 +3,7 @@
 iperf -s &
 pid=$!
 
-for n in `seq 1 6`; do
+for n in `seq 1 7`; do
   ssh 10.1.1.$n iperf -c 10.1.1.200 -t 60 > /dev/null
 done
 
--- ping.sh
+++ ping.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-for n in `seq 1 6`; do
+
+for n in `seq 1 7`; do
   ping -q -w 60 10.1.1.$n
 done
